b4243ab182b1acbe2667b2a8ce5e21e91d89621a
Umang <umang.rastogi96@gmail.com>
2020-02-23 21:10:03 -0500

    Add functions to get orientation and tag ID

--- Code/detector.py
+++ Code/detector.py
@@ -1,49 +1,71 @@
 import cv2 as cv
 
 
-def draw_contours(img_frame):
-    """  find and draw contours on the main frame
+def find_contours(img_frame):
+    """
+    find and draw contours on the main frame
     :param img_frame: a frame from the video
     :return: image with contours and the contours matrix
     """
-    _, frame_thresh = cv.threshold(cv.cvtColor(img_frame, cv.COLOR_BGR2GRAY), 200, 255, 0)
+    _, frame_thresh = cv.threshold(cv.cvtColor(img_frame, cv.COLOR_BGR2GRAY), 220, 255, 0)
     contours, _ = cv.findContours(frame_thresh, cv.RETR_TREE, cv.CHAIN_APPROX_SIMPLE)
     return contours
 
 
-def get_h_matrix(orientation, x, y):
-    """ get H-matrix for detected orientation
-    :param orientation: orientation from get orientation function
-    :param x:
-    :param y:
-    :return:
-    """
-    orientations = ['top_right', 'top_left', 'bottom_right', 'bottom_left']
-    # TODO: return H-matrix not orientations
-    # Returning orientations only for stub implementation
-    return orientations
-
-
-def get_orientation(input_img):
+def get_orientation(img_frame):
     """ get orientation from the image frame
-    :param input_img: image frame from the video
+    :param img_frame: image frame from the video
     :return: orientation of the tag
     """
-
-    # TODO: implement algorithm to deduce orientation from input image
-    orientations = ['top_right', 'top_left', 'bottom_right', 'bottom_left']
-    # TODO: return correct orientation after deduction
+    # Check get_H_matrix function in superimpose.py for orientation notation
+    orientations = {0: 0, 1: 0, 2: 0, 3: 0}
+    # Orientation: Bottom Right
+    for i in range(250, 301):
+        for j in range(250, 301):
+            orientations[0] += img_frame[i, j]
+    # Orientation: Bottom Left
+    for i in range(250, 301):
+        for j in range(100, 151):
+            orientations[1] += img_frame[i, j]
+    # Orientation: Top Right
+    for i in range(100, 151):
+        for j in range(250, 301):
+            orientations[2] += img_frame[i, j]
+    # Orientation: Top Left
+    for i in range(100, 151):
+        for j in range(100, 151):
+            orientations[3] += img_frame[i, j]
     # Returning top-right as orientation only for stub implementation
-    return orientations[0]
+    return max(orientations, key=orientations.get)
 
 
-def get_tag_td(input_img, orientation):
+def get_tag_id(img_frame, orientation):
     """
-    :param input_img:
+    :param img_frame:
     :param orientation:
     :return:
     """
+    tag_id = ''
+    keys = []
+    # Check get_H_matrix function in superimpose.py for orientation notation
+    if orientation == 0:
+        keys = [1, 0, 2, 3]
+    elif orientation == 1:
+        keys = [3, 1, 0, 2]
+    elif orientation == 2:
+        keys = [2, 3, 1, 0]
+    elif orientation == 3:
+        keys = [0, 2, 3, 1]
+    structure = {0: [200, 250, 200, 250], 1: [150, 200, 200, 250], 2: [200, 250, 150, 200], 3: [150, 200, 150, 200]}
 
-    # TODO: implement algorithm to deduce orientation from input image
-    tag_id = orientation
+    total = 0
+    for key in keys:
+        for i in range(structure[key][0], structure[key][1]):
+            for j in range(structure[key][2], structure[key][3]):
+                total += img_frame[i][j]
+
+        if (total / 2500) > 180:
+            tag_id += '1'
+        else:
+            tag_id += '0'
     return tag_id
