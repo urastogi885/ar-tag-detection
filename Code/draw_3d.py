950d66a12d5113d448b7422f57ab26db1f6fe9b0
Umang <umang.rastogi96@gmail.com>
2020-02-25 02:24:02 -0500

    Write function to get k,r,t matrices

--- Code/draw_3d.py
+++ Code/draw_3d.py
@@ -1,6 +1,28 @@
-def get_krt_matrix():
-    return
+import numpy as np
 
 
-def draw_cuboid():
-    return
+def get_krt_matrix(inv_h):
+    k_mat = np.array(
+        [[1406.08415449821, 0, 0], [2.20679787308599, 1417.99930662800, 0], [1014.13643417416, 566.347754321696, 1]]).T
+    inv_k_mat = np.linalg.inv(k_mat)
+    b_mat = np.matmul(inv_k_mat, inv_h)
+    b1 = b_mat[:, 0].reshape(3, 1)
+    b2 = b_mat[:, 1].reshape(3, 1)
+    r3 = np.cross(b_mat[:, 0], b_mat[:, 1])
+    b3 = b_mat[:, 2].reshape(3, 1)
+    scalar = 2 / (np.linalg.norm(inv_k_mat.dot(b1)) + np.linalg.norm(inv_k_mat.dot(b2)))
+    t = scalar * b3
+    r1 = scalar * b1
+    r2 = scalar * b2
+    r3 = (r3 * scalar * scalar).reshape(3, 1)
+    r_mat = np.concatenate((r1, r2, r3), axis=1)
+    return r_mat, t, k_mat
+
+
+# def draw_cuboid(video_frame, three_d_points):
+#     three_d_points = np.int32(three_d_points).reshape(-1, 2)
+#     video_frame = cv.drawContours(video_frame, [three_d_points[:4]], -1, (0, 255, 0), 3)   # Ground plane
+#     for i, j in zip(range(4), range(4, 8)):                                    # Z Axis planes
+#         video_frame = cv.line(video_frame, tuple(three_d_points[i]), tuple(three_d_points[j]), (0, 0, 255), 3)
+#     video_frame = cv.drawContours(video_frame, [three_d_points[4:]], -1, (255, 0, 0), 3)   # Top plane
+#     return video_frame
